6fecde34837923c19b338d103fc3d457305eb449
wangchaoyong <1357773401@qq.com>
2019-08-22 17:36:47 +0800

    log4j2 修改输出日志文件名

--- src/main/java/com/sohu/spaces/version/web/servlet/LogDemo.java
+++ src/main/java/com/sohu/spaces/version/web/servlet/LogDemo.java
@@ -1,7 +1,5 @@
 package com.sohu.spaces.version.web.servlet;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,11 +7,14 @@ public class LogDemo {
 
     private static Logger logger = LoggerFactory.getLogger(LogDemo.class);;
     public static void main(String[] args) {
-        logger.warn("warn xsfsdf");
-        logger.info("info xinxi");
-        java.util.logging.Logger logDemo = java.util.logging.Logger.getLogger("LogDemo");
-        logDemo.info("jul info");
-        Log log = LogFactory.getLog(LogDemo.class);
-        log.info("jcl info");
+        for (int i = 0; i < 500000 ; i++) {
+            logger.warn("warn xsfsdf");
+            logger.info("info xinxi");
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
