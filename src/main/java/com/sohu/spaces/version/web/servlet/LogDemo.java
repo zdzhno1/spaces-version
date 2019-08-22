2f837a40afd1ef821ffe612a41d5eae841f0b681
wangchaoyong <1357773401@qq.com>
2019-08-22 15:31:27 +0800

    initial log4j2+slf4j

---
+++ src/main/java/com/sohu/spaces/version/web/servlet/LogDemo.java
@@ -0,0 +1,13 @@
+package com.sohu.spaces.version.web.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogDemo {
+
+    private static Logger logger = LoggerFactory.getLogger(LogDemo.class);;
+    public static void main(String[] args) {
+        logger.warn("warn xsfsdf");
+        logger.info("info xinxi");
+    }
+}
